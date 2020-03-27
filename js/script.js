fde4238bf750aebd4d1141598945593831ec12cc
Fagner Lima <contato@fagnerlima.pro.br>
2020-03-27 19:02:06 -0300

    Formata página de Contato

--- js/script.js
+++ js/script.js
@@ -1,39 +1,41 @@
-/**
- * Menu principal (hamburguer button)
- */
-var buttonMainMenu = $('nav.main button.main-menu');
-var ulMainMenu = $('nav.main ul.main-menu');
-var siteMask = $('#site-mask');
+$(document).ready(function() {
+  /**
+   * Menu principal (hamburguer button)
+   */
+  var buttonMainMenu = $('nav.main button.main-menu');
+  var ulMainMenu = $('nav.main ul.main-menu');
+  var siteMask = $('#site-mask');
 
-function showMainMenu() {
-  ulMainMenu.slideToggle();
-  siteMask.toggle();
-}
-
-function hideMainMenu() {
-  if (buttonMainMenu.css('display') == 'none') {
-    return;
+  function showMainMenu() {
+    ulMainMenu.slideToggle();
+    siteMask.toggle();
   }
 
-  ulMainMenu.slideUp();
-  siteMask.fadeOut(1000);
-}
+  function hideMainMenu() {
+    if (buttonMainMenu.css('display') == 'none') {
+      return;
+    }
 
-buttonMainMenu.click(showMainMenu);
-ulMainMenu.find('a').click(hideMainMenu);
-siteMask.click(hideMainMenu);
+    ulMainMenu.slideUp();
+    siteMask.fadeOut(1000);
+  }
 
-/**
- * Efeito parallax
- */
-$('.scroll').click(function(event) {
-  event.preventDefault();
-  $('html, body').animate({
-    scrollTop: $(this.hash).offset().top
-  }, 800);
+  buttonMainMenu.click(showMainMenu);
+  ulMainMenu.find('a').click(hideMainMenu);
+  siteMask.click(hideMainMenu);
+
+  /**
+   * Efeito parallax
+   */
+  $('.scroll').click(function(event) {
+    event.preventDefault();
+    $('html, body').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 800);
+  });
+
+  /**
+   * Actual year
+   */
+  $('.actual-year').html((new Date()).getFullYear());
 });
-
-/**
- * Actual year
- */
-$('.actual-year').html((new Date()).getFullYear());
