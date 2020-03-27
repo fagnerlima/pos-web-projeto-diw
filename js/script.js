ef4f18729c71fe12d9c50a50a16492ce483dfb06
Fagner Lima <contato@fagnerlima.pro.br>
2020-03-27 10:56:16 -0300

    Adiciona arquivos do layout

---
+++ js/script.js
@@ -0,0 +1,39 @@
+/**
+ * Menu principal (hamburguer button)
+ */
+var buttonMainMenu = $('nav.main button.main-menu');
+var ulMainMenu = $('nav.main ul.main-menu');
+var siteMask = $('#site-mask');
+
+function showMainMenu() {
+  ulMainMenu.slideToggle();
+  siteMask.toggle();
+}
+
+function hideMainMenu() {
+  if (buttonMainMenu.css('display') == 'none') {
+    return;
+  }
+
+  ulMainMenu.slideUp();
+  siteMask.fadeOut(1000);
+}
+
+buttonMainMenu.click(showMainMenu);
+ulMainMenu.find('a').click(hideMainMenu);
+siteMask.click(hideMainMenu);
+
+/**
+ * Efeito parallax
+ */
+$('.scroll').click(function(event) {
+  event.preventDefault();
+  $('html, body').animate({
+    scrollTop: $(this.hash).offset().top
+  }, 800);
+});
+
+/**
+ * Actual year
+ */
+$('.actual-year').html((new Date()).getFullYear());
